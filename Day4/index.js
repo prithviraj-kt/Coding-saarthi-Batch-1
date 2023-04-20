e9a64facdfedded6dc0f40d48c777a9f805363b6
prithvirajktagadinamani@gmail.com <prithvirajktagadinamani@gmail.com>
2023-04-20 21:48:46 +0530

    Day 4

---
+++ Day4/index.js
@@ -0,0 +1,99 @@
+// let, const, var
+
+let number = 1;
+
+function name() {
+  let name = "harry";
+  const num = 10;
+  var usn = 10001010;
+}
+
+// console.log(name)
+// console.log(num)
+// console.log(usn)
+// YOu cannot redeclare let variable
+// let number = "name"
+// let str = "Hello"
+// let arr = [1, 2, 3]
+// let myDictionary = {
+//     "name":"Prithviraj",
+//     "usn":"1001001010",
+//     "gmail":{
+//         "firstEmail":"abc@gmail.com",
+//         "secondEmail":"xyz@gmail.com"
+//     }
+// }
+
+// console.log(myDictionary.gmail['firstEmail'])
+
+// console.log(myDictionary.gmail[0])
+
+// let add = 10 + 20
+// let sub = 20 - 10
+// let mul = 10 * 10
+// let div = 20 / 10
+// let pow = 20 ** 20
+// let add2 = add++
+// let sub2 = sub--
+
+// const d = new Date("2021-03-25");
+// console.log(d.getMonth());
+
+// let a = 10
+
+// if (a>10){
+//     console.log("Limit exceeded")
+// }
+// else{
+//     console.log("Its perfect")
+// }
+
+// for (let i = 0; i< 5;  i++){
+//     console.log(i)
+// }
+
+
+
+// FOR LOOP ( FOR IN ) USED ONLY WITH OBJECTS
+
+
+// const data = {
+//   "fname": "Prithviraj",
+//   "lname": "Tagadinamani",
+//   "phone": "9363968791",
+//   "email": "prithvirajktagadinamani@gmail.com",
+// };
+
+// for (i in data) {
+//   console.log(data[i]);
+// }
+
+
+
+
+// FOR LOOP ( FOR OF ) USED ONLY WITH ARRAY
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (let i of arr){
+//     console.log(i)
+// }
+
+// a = 11
+
+// while(a>10){
+//     console.log("Hello")
+//     a--
+// }
+
+// var a = "0"
+// var b = 0
+// if (parseInt(a) == b){
+//     console.log("Values are equal")
+// }else{
+//     console.log("values are not equal")
+// }
+
+
+
+// console.log(typeof(a))
